cf96f4c9bf2b7dc9313770e4723658f1d2003ec4
alexgutowski05 <57464997+alexgutowski05@users.noreply.github.com>
2020-05-18 06:58:28 -0400

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,3 +1,38 @@
+function instagram() {
+	var redirect = confirm("You are leaving amgutowski.com and heading to a trusted external source.");
+	if (redirect == true) {
+		window.open("https://www.instagram.com/alex.the.potato.05", "_blank");
+	} 
+}
+
+function twitter() {
+	var redirect = confirm("You are leaving amgutowski.com and heading to a trusted external source.");
+	if (redirect == true) {
+		window.open("https://www.twitter.com/alexthepotato05", "_blank");
+	} 
+}
+
+function youtube() {
+	var redirect = confirm("You are leaving amgutowski.com and heading to a trusted external source.");
+	if (redirect == true) {
+		window.open("https://www.youtube.com/channel/UC0BvvbmIDaZYSTToUFG-wbg?view_as=subscriber", "_blank");
+	} 
+}
+
+function vimeo() {
+	var redirect = confirm("You are leaving amgutowski.com and heading to a trusted external source.");
+	if (redirect == true) {
+		window.open("https://www.vimeo.com/alexgutowski", "_blank");
+	} 
+}
+
+function stack() {
+	var redirect = confirm("You are leaving amgutowski.com and heading to a trusted external source.");
+	if (redirect == true) {
+		window.open("https://www.stackoverflow.com/users/12258724/alex-gutowski", "_blank");
+	}
+}
+
 var xhr= new XMLHttpRequest();	
 xhr.open('GET', 'nav.html', true);
 xhr.onreadystatechange= function() {
