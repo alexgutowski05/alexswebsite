2ad5d3b569bb713463bd5c607021c41aa9c40840
alexgutowski05 <57464997+alexgutowski05@users.noreply.github.com>
2020-06-08 15:23:24 -0400

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -33,6 +33,13 @@ function stack() {
 	}
 }
 
+function lawnCare() {
+	var redirect = confirm("You are being redirected to checkout.square.site to complete your purchace. This payment is made to Alex Gutowski.");
+	if (redirect == true) {
+		window.open("https://checkout.square.site/buy/KXV44DDQSUSUX4HIZQFZYGYG", "_self");
+	}
+}
+
 var xhr= new XMLHttpRequest();	
 xhr.open('GET', 'nav.html', true);
 xhr.onreadystatechange= function() {
