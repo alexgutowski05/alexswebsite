06de2896a627e2c9c23e524cdbb2e1f7e402099c
alexgutowski05 <57464997+alexgutowski05@users.noreply.github.com>
2021-12-18 20:07:49 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,8 +1,14 @@
-var xhr= new XMLHttpRequest();	
+if (!window.XMLHttpRequest && 'ActiveXObject' in window) {
+    window.XMLHttpRequest= function() {
+        return new ActiveXObject('MSXML2.XMLHttp');
+    };
+}
+
+var xhr= new XMLHttpRequest();
 xhr.open('GET', 'nav.html', true);
 xhr.onreadystatechange= function() {
-	if (this.readyState!==4) return;
-	if (this.status!==200) return; // or whatever error handling you want
-	document.getElementById('nav').innerHTML= this.responseText;
+    if (this.readyState!==4) return;
+    if (this.status!==200) return; // or whatever error handling you want
+    document.getElementById('nav').innerHTML= this.responseText;
 };
 xhr.send();
